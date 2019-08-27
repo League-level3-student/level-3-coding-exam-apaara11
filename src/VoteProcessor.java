8b715f9622b51615deb855f86d387b7923dbb035
League Student <league@iMac-24.attlocal.net>
2019-08-26 18:50:46 -0700

    Exam (IP)

--- src/VoteProcessor.java
+++ src/VoteProcessor.java
@@ -1,4 +1,49 @@
+import java.util.ArrayList;
 
-public class VoteProcessor {
+public class VoteProcessor  {
 
+	
+
+
+private static final String voters = null;
+
+public String calculateElectionWinner(ArrayList<String> votes) {
+	// TODO Auto-generated method stub
+	int scoreF =0;
+	int scoreE =0;
+	Level3CodingExam l = new Level3CodingExam();
+	
+	for(int i=0; i<votes.size();i++) {
+		if(voters.equals("pope francis")){
+			scoreF+=1;
+			l.testWinner();
+		}
+//		else if(votes.equals("Pope Francis")) {
+//			scoreF+=1;
+//		}
+//		else if(votes.equals("Pope francis")) {
+//			scoreF+=1;
+//		}
+//		else if(votes.equals("pope Francis")) {
+//			scoreF+=1;
+//		}
+//		
+//		else {
+//			scoreE+=1;
+//		}
+//	
+//	if(scoreF>scoreE) {
+//		System.out.println("Francis won");
+//	}
+//	else if(scoreF==scoreE) {
+//		testATie();
+//	}
+//	else {
+//		System.out.println("Other Guy won");
+//	}
+	
+	
+	
+	return null;
+}
 }
